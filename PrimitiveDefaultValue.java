63d01c1fd8fce530ad3c068784e53750d8e845ee
bridgelab <surajsamal19@gmail.com>
2023-06-15 19:59:52 +0530

    File added successfully

---
+++ PrimitiveDefaultValue.java
@@ -0,0 +1,17 @@
+public class PrimitiveDefaultValue{
+static int val1;
+static long val2;
+static float val3;
+static double val4;
+static char val5;
+static boolean val6;
+static byte val7;
+public static void main(String args[]){
+System.out.println("The value of int is" +val1);
+System.out.println("The value of long is" +val2);
+System.out.println("The value of float is"+val3);
+System.out.println("The vlaue of double is"+val4);
+System.out.println("The value of char is" +val5);
+System.out.println("The value of boolean is "+val6);
+System.out.println("The vlaue of byte is" +val7);
+}}
