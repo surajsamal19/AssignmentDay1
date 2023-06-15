236463b2d910706942739bcf71c69e57914935bf
bridgelab <surajsamal19@gmail.com>
2023-06-15 19:58:22 +0530

    File added succesfully

---
+++ GetCommandName.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class GetCommandName{
+public static void main(String args[]){
+Scanner input = new Scanner(System.in);
+System.out.println("enter your Name");
+String name = input.nextLine();
+System.out.println("Hello  "    +name+    "    ");
+}
+}
